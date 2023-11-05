4e92187cf9d96df5351f9b6eb4501ef31fac5699
codewriterX <miturugi.infinite@gmail.com>
2023-11-05 19:46:30 +0900

    修正後

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 const titleName = '===============================\n現在持っているタスク一覧\n===============================';
 
-const taskObj = [{genre:'掃除',taskName:'机を片付ける'}, {genre:'買い物',taskName:'牛乳を買う'}, {genre:'運動',taskName:'散歩する'}];
+const tasks = [{genre:'掃除',taskName:'机を片付ける'}, {genre:'買い物',taskName:'牛乳を買う'}, {genre:'運動',taskName:'散歩する'}];
 
 const add = () => {
     console.log(titleName);
 
-    for (let i = 0; i < taskObj.length; i++) {
-        const task = taskObj[i];
-        const taskName_value = task.genre;
-        const taskDescription = task.taskName;
-        console.log(`${i}: [内容] ${taskName_value}、[ジャンル] ${taskDescription}`);
-    }
+    tasks.forEach (function(value,index) {
+        console.log(`${index}:[内容]:${value.taskName},[ジャンル]:${value.genre}`);
+    });
 
     const comformDialog = prompt('「確認、追加、削除、終了のいずれかを入力してください。」');
     if (comformDialog === '追加') {
         const taskPrompt = prompt('タスクを入力してください。');
         const genrePrompt = prompt('ジャンルを入力してください。');
         const newTask = {genre:genrePrompt,taskName:taskPrompt};
-        taskObj.push(newTask);
+        tasks.push(newTask);
 
         console.log(titleName);
-        for (let i = 0; i < taskObj.length; i++) {
-            const task = taskObj[i];
-            const taskName_value = task.taskName;
-            const taskDescription = task.genre;
-            console.log(`${i}: [内容] ${taskName_value}、[ジャンル] ${taskDescription}`);
-        }
+        tasks.forEach (function(value,index){
+            console.log(`${index}:[内容]:${value.taskName},[ジャンル]:${value.genre}`);
+        });
     }
 };
 
